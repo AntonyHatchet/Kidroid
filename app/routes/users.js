4948715b6f291b7f6b411e7125f6848a8036f008
Anton Gorshenin <anton_gorshenin@etb-lab.com>
2015-04-24 17:59:26 +0300

    jade

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,17 +3,10 @@ module.exports = {
 
   // Get shop home page
   getDevice: function(req, res) {
-      //console.log(req);
-    deviceMagic.getAllDevice(function(err, Device){
+    deviceMagic.getAllDevice(function(err, Devices){
       if (err) {console.log(err);}
-        res.render('dashboard.jade', {
-            Timestamp: Device.Timestamp,
-            device_id: Device.device_id,
-            token: Device.token,
-            latitude: Device.latitude,
-            longitude: Device.longitude,
-            apk_version: Device.apk_version,
-            loader_version: Device.loader_version
+        res.render('dashboard', {
+            devices: Devices
         });
     })
 
