cdd8689a6cea34dc55209ac11bb8db591e960d89
Anton Gorshenin <anton_gorshenin@etb-lab.com>
2015-04-24 15:28:10 +0300

    Session Stores

--- app/routes.js
+++ app/routes.js
@@ -21,8 +21,8 @@ module.exports = function(app, passport) {
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/', // redirect to the secure profile section
-        failureRedirect : '/failure', // redirect back to the signup page if there is an error
+        successRedirect : '/dashboard', // redirect to the secure profile section
+        failureRedirect : '/', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
@@ -32,8 +32,7 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/dashboard', isLoggedIn, function(req, res) {
-        res.render('dashboard.jade', {
-            user : req.user // get the user out of session and pass to template
+        res.render('dashboard.jade', { user : req.user, message: req.flash('loginMessage') // get the user out of session and pass to template
         });
     });
 
@@ -48,7 +47,6 @@ module.exports = function(app, passport) {
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-
     // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
