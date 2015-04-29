1624878564cc71ef70c361d7bcac5fc355929775
Anton Gorshenin <anton_gorshenin@etb-lab.com>
2015-04-29 20:17:04 +0300

    DB remoute

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 /**
  * Created by anton_gorshenin on 23.04.2015.
  */
-//'url': 'mongodb://tester:tester@ds039880.mongolab.com:39880/kidroid'
+// 'url': 'mongodb://172.30.116.209:27017/kidroid'
 module.exports = {
-    'url': 'mongodb://172.30.116.209:27017/kidroid'
+    'url': 'mongodb://tester:tester@ds039880.mongolab.com:39880/kidroid'
 };
