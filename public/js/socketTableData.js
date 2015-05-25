c401e6cc0363796fd24b87b84bccda30599de88d
nikolay_ivinisenko <nikolay_ivanisenko@etb-lab.com>
2015-05-25 21:59:47 +0300

    filtre

--- public/js/socketTableData.js
+++ public/js/socketTableData.js
@@ -8,6 +8,17 @@
         $("#userTable").html(html);
     });
 
-        socket.on('quantity', function (data) {
-            console.log(data);
+        socket.on('quantity', function (pg) {
+            html = '';
+            Page=Math.ceil(pg/10);
+            for (var j = 1; j <=Page ; j++)
+                html += "<li><a onclick='socket.emit(\"pageNumder\"," + j*10 + ")'>" + j + "</a></li>";
+            $("#pagination").html(html);
+    });
+
+        socket.on('category', function (school) {
+            html = '';
+            for (i in school)
+                html += "<tr><td>"+school[i].device_id+"</td><td><a onclick=\"socket.emit('categoryDel')\")>remove</a> / <a href='editCategory'>edit</a></td></tr>";
+            $("#tableFilter").html(html);
     });
