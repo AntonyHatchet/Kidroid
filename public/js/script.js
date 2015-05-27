936d1dc188687ea5dba0b700dd5dc57be5217a27
nikolay_ivinisenko <nikolay_ivanisenko@etb-lab.com>
2015-05-27 20:38:38 +0300

    pageFind1

--- public/js/script.js
+++ public/js/script.js
@@ -35,7 +35,7 @@ function page(i) {
     device.status = $("#selectStatus").val();
     device.category = $("#selectCategory").val();
     device.version = $("#selectVersion").val();
-    device.page = i;
+    device.page = i*10;
     socket.emit('getDevicesByParams', device);
     console.log(device);
 };
