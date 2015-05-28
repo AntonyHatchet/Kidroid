d855bdb0340013cff701a03631d05d0fcd8c5fcb
nikolay_ivinisenko <nikolay_ivanisenko@etb-lab.com>
2015-05-28 12:14:15 +0300

    style

--- public/js/socketTableData.js
+++ public/js/socketTableData.js
@@ -5,7 +5,7 @@ socket.on('displayData', function (data) {
     console.log(data);
     html = '';
     for (i in data)
-        html += "<tr><td>" + data[i].device_id + "</td><td>" + data[i].registered + "</td><td>" + data[i].school + "</td><td>" + data[i].apk_version + "</td><td><button class='btn' onclick='showmap(" + data[i].longitude + "," + data[i].latitude + ")'>show map</button></td><td><button class='btn' type='button' onclick=\'socket.emit(\"removeDevice\",\"" + data[i].device_id + "\")\')>delete</button></td></tr>";
+        html += "<tr><td>" + data[i].device_id + "</td><td>" + data[i].registered + "</td><td>" + data[i].school + "</td><td>" + data[i].apk_version + "</td><td><button class='btn btn-default' onclick='showmap(" + data[i].longitude + "," + data[i].latitude + ")'>show map</button></td><td><button class='btn btn-default' type='button' onclick=\'socket.emit(\"removeDevice\",\"" + data[i].device_id + "\")\')>delete</button></td></tr>";
     $("#deviceTable").html(html);
 });
 
@@ -21,14 +21,14 @@ socket.on('category', function (school) {
     //console.log(school,"category");
     html = '';
     for (var i = 0; i < school.length; i++) {
-        html += "<tr><td style=\'display: none\'>" + school[i]._id + "</td><td>" + school[i].name + "</td><td><button class='btn' type='button' onclick=\'socket.emit(\"removeCategory\",\"" + school[i]._id + "\")\')>remove</button> / <a href=\'#editCategory\'><button class='btn'>edit</button></a></td></tr>";
+        html += "<tr><td style=\'display: none\'>" + school[i]._id + "</td><td>" + school[i].name + "</td><td><button class='btn btn-default' type='button' onclick=\'socket.emit(\"removeCategory\",\"" + school[i]._id + "\")\')>remove</button> / <a href=\'#editCategory\'><button class='btn btn-default'>edit</button></a></td></tr>";
     }
     $("#tableFilter").html(html);
 });
 
 socket.on('category', function (date) {
     //console.log(date, "category");
-    html = '<option value="">Select school</option>';
+    html = '<option value="" style="color:#cccccc">Select school</option>';
     for (var i = 0; i < date.length; i++) {
         html += "<option>" + date[i].name + "</option>";
     }
@@ -37,7 +37,7 @@ socket.on('category', function (date) {
 
 socket.on('version', function (date) {
     //console.log(school,"category");
-    html = '<option value="">Select version</option>';
+    html = '<option value="" style="color:#cccccc">Select version</option>';
     for (var i = 0; i < date.length; i++) {
         html += "<option>" + date[i].version_apk + "</option>";
     }
@@ -48,7 +48,7 @@ socket.on('users', function (data) {
     console.log(data);
     html = '';
     for (i in data)
-        html += "<tr><td>" + data[i]._id + "</td><td>" + data[i].local.name + "</td></tr>";
+        html += "<tr><td>" + data[i]._id + "</td><td>" + data[i].local.name + "</td><td><button class='btn btn-default' type='button' onclick=\'socket.emit(\"removeUsers\",\"" + data[i]._id + "\")\')>delete</button></td></tr>";
     $("#userTable").html(html);
 });
 
