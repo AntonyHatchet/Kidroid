54e34360426b67cf8baa71ebf355c297c7cf4f6e
Anton Gorshenin <anton_gorshenin@etb-lab.com>
2015-06-23 14:39:20 +0300

    Сохраняем историю перемещений

--- public/js/socketTableData.js
+++ public/js/socketTableData.js
@@ -224,7 +224,6 @@ socket.on('filters', function (data) {
                 var checkbox = "<td><input type='checkbox' class='checkAllFilters' id='checkSchedule'  value='"+ data[i].params[f] +"'></td>"
                 html += "<tr>" + checkbox + name + editButton + "</tr>";
             }
-            startAutoComplete(filter2.getArray(),".filter2");
             $("#filtersTable").html(html);
         }
     }
